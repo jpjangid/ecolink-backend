5d5a52eaeff71892447cdd9b5a262e54d6aac91f
WebAnixUdaipur <info.webanix@gmail.com>
2022-03-16 06:44:40 +0000

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,6 +3,6 @@
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>Developed by</b>
-        <a href="https://www.webanix.in/" target="blank">WEBanix</a>
+        <a href="https://www.fusionfirst.com/" target="blank">FBSPL</a>
     </div>
 </footer>
