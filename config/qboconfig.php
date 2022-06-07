e27c1b2be9276129112ea4b7712b25228c9b2e0f
rishabhjainofficial <101650802+rishabhjainofficial@users.noreply.github.com>
2022-06-07 20:57:57 +0530

    changes in order for third api get main data from config file

--- config/qboconfig.php
+++ config/qboconfig.php
@@ -1,10 +1,9 @@
 <?php
 
 return [
-    'authorizationRequestUrl' => 'https://appcenter.intuit.com/connect/oauth2',
-    'tokenEndPointUrl' => 'https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer',
     'client_id' => 'AB0xOsnrC6RaG9icSOdAlyQvcf08pF3zdMeKic9qr9pLHe7TPP',
     'client_secret' => 'HtRdl7YtY70YINIufv1LaLqvivfGFiNgUmEoU7vh',
-    'oauth_scope' => 'com.intuit.quickbooks.accounting openid profile email phone address',
-    'oauth_redirect_uri' => 'http://localhost:3000/callback.php',
+    'company_id' => '4620816365226953830',
+    'url' => 'https://sandbox-quickbooks.api.intuit.com/',
+    'minorversion' => '65'
 ];
