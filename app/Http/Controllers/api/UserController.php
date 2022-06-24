5be34c485b1df988b7d29984842ba2b28380a12b
Ravindra Singh Panwar <ravindra@webanix.in>
2022-06-24 15:57:21 +0530

    Feedback on call

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -338,6 +338,7 @@ class UserController extends Controller
       $user->pincode          =   $request['pincode'];
       $user->password         =   $pass;
       $user->profile_image    =   $image_name;
+      $user->landmark         =   $request['landmark'];
       $user->save();
       
       $user->profile_image = asset('storage/profile_image/' . $user->profile_image);
